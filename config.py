e35580c05ddd1fd591f36e0777c422088b39b24b
Ruslana Onyshchuk <lalelya7@gmail.com>
2025-05-05 11:04:43 +0200

    added static domain

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 API_TOKEN = os.getenv("BOT_TOKEN")
 
-WEBHOOK_HOST = 'https://a41c-2a13-8a02-620e-ef00-e433-da1a-475a-ed56.ngrok-free.app'
+WEBHOOK_HOST = 'https://generous-crow-lightly.ngrok-free.app'
 WEBHOOK_PATH = '/webhook'
 WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 
